c6a83dc492bf25a8c5c967d3e7c05c7c97eadac1
Gabriel de Carvalho Antunes <gabrielca.pro@gmail.com>
2026-01-09 16:09:09 -0300

    feat: adicionado array como state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
-const App = () => {
-    return(
-        <div className="App">
-            <h1>Hello DIO!</h1>
-        </div>
-    )
-};
+import React, { Component } from "react";
+
+class App extends Component {
+    state = {
+        usuarios: ["Gabriel", "Vagner", "Michele"]
+    };
+
+    render() {
+        const { usuarios } = this.state;
+        return(
+            <div className="App">
+                <h1>Hello DIO!</h1>
+                {usuarios.map((item) => (
+                    <p>{item}</p>
+                ))}
+            </div>
+        )
+    }
+}
 
 export default App;
